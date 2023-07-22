278b2d98c59931c70275af780d8724e8a33d170f
wudijimao <wudijimao@qq.com>
2023-07-22 22:06:04 +0800

    fix: download must be wget, support coutinue download

--- download.py
+++ download.py
@@ -7,30 +7,50 @@ import os
 import requests
 import hashlib
 
+def wget(url, file_path):
+    wget_fake_header = {'User-Agent': 'Wget/1.20.3 (linux-gnu)'}
+    temp_file_path = file_path + '.tmp'
+    file_size = 0
+    if os.path.exists(file_path):
+        return file_path
+    elif os.path.exists(temp_file_path):
+        file_size = os.path.getsize(temp_file_path)
+        headers = {'Range': f'bytes={file_size}-', 'User-Agent': 'Wget/1.20.3 (linux-gnu)'}
+        response = requests.get(url, headers=headers, stream=True)
+        mode = 'ab'
+    else:
+        response = requests.get(url, headers=wget_fake_header, stream=True)
+        mode = 'wb'
+    total_size = int(response.headers.get('Content-Length', 0))
+    block_size = 8192
+    wrote = file_size
+    with open(temp_file_path, 'wb') as f:
+        f.seek(0, os.SEEK_END)
+        for chunk in response.iter_content(chunk_size=block_size):
+            if chunk:
+                f.write(chunk)
+                wrote += len(chunk)
+                progress = int(50 * wrote / total_size)
+                print('\r[{}{}] {:.1f}%'.format('#' * progress, ' ' * (50 - progress), 100 * wrote / total_size), end='')
+    os.rename(temp_file_path, file_path)
+    return file_path
+
 presigned_url = input("Enter the URL from email: ")
 print("")
 model_size = input("Enter the list of models to download without spaces (7B,13B,70B,7B-chat,13B-chat,70B-chat), or press Enter for all: ")
-target_folder = ".\models"             # where all files should end up
+target_folder = "."             # where all files should end up
 os.makedirs(target_folder, exist_ok=True)
 
 if model_size == "":
     model_size = "7B,13B,70B,7B-chat,13B-chat,70B-chat"
 
 print("Downloading LICENSE and Acceptable Usage Policy")
-license_response = requests.get(presigned_url.replace('*', "LICENSE"))
-with open(os.path.join(target_folder, "LICENSE"), 'wb') as license_file:
-    license_file.write(license_response.content)
-policy_response = requests.get(presigned_url.replace('*', "USE_POLICY.md"))
-with open(os.path.join(target_folder, "USE_POLICY.md"), 'wb') as policy_file:
-    policy_file.write(policy_response.content)
+wget(presigned_url.replace('*', "LICENSE"), os.path.join(target_folder, "LICENSE"))
+wget(presigned_url.replace('*', "USE_POLICY.md"), os.path.join(target_folder, "USE_POLICY.md"))
 
 print("Downloading tokenizer")
-tokenizer_model_response = requests.get(presigned_url.replace('*', "tokenizer.model"))
-with open(os.path.join(target_folder, "tokenizer.model"), 'wb') as tokenizer_model_file:
-    tokenizer_model_file.write(tokenizer_model_response.content)
-tokenizer_checklist_response = requests.get(presigned_url.replace('*', "tokenizer_checklist.chk"))
-with open(os.path.join(target_folder, "tokenizer_checklist.chk"), 'wb') as tokenizer_checklist_file:
-    tokenizer_checklist_file.write(tokenizer_checklist_response.content)
+wget(presigned_url.replace('*', "tokenizer.model"), os.path.join(target_folder, "tokenizer.model"))
+wget(presigned_url.replace('*', "tokenizer_checklist.chk"), os.path.join(target_folder, "tokenizer_checklist.chk"))
 
 def check_md5(file_path, checksum):
     with open(file_path, 'rb') as f:
@@ -74,17 +94,10 @@ for model in model_size.split(','):
     os.makedirs(os.path.join(target_folder, model_path), exist_ok=True)
 
     for s in range(shard + 1):
-        consolidated_response = requests.get(presigned_url.replace('*', f"{model_path}/consolidated.{s:02d}.pth"))
-        with open(os.path.join(target_folder, model_path, f"consolidated.{s:02d}.pth"), 'wb') as consolidated_file:
-            consolidated_file.write(consolidated_response.content)
+        wget(presigned_url.replace('*', f"{model_path}/consolidated.{s:02d}.pth"), os.path.join(target_folder, model_path, f"consolidated.{s:02d}.pth"))
 
-    params_response = requests.get(presigned_url.replace('*', f"{model_path}/params.json"))
-    with open(os.path.join(target_folder, model_path, "params.json"), 'wb') as params_file:
-        params_file.write(params_response.content)
-    checklist_response = requests.get(presigned_url.replace('*', f"{model_path}/checklist.chk"))
-    with open(os.path.join(target_folder, model_path, "checklist.chk"), 'wb') as checklist_file:
-        checklist_file.write(checklist_response.content)
+    wget(presigned_url.replace('*', f"{model_path}/params.json"), os.path.join(target_folder, model_path, "params.json"))
+    wget(presigned_url.replace('*', f"{model_path}/checklist.chk"), os.path.join(target_folder, model_path, "checklist.chk"))
     
     print("Checking checksums")
     check_checksums(os.path.join(target_folder, model_path), os.path.join(target_folder, model_path, "checklist.chk"))
-
